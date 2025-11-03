e1faf95d366307d09c134ac3ac100717ddeada3d
Nesterov Nikita <nesnikm@gmail.com>
2025-11-03 16:54:37 +0300

    Fourth Commit (Homework #8)

--- tests/test_visible.py
+++ tests/test_visible.py
@@ -1,11 +1,11 @@
 from pages.elements_page import ElementsPage
 import time
-
-def test_visible_btn_sidebar(browser):
-    elements_page = ElementsPage(browser)
-
-    elements_page.visit()
-    assert elements_page.btn_sidebar_first_textbox.visible()
+#
+# def test_visible_btn_sidebar(browser):
+#     elements_page = ElementsPage(browser)
+#
+#     elements_page.visit()
+#     assert elements_page.btn_sidebar_first_textbox.visible()
 
 def test_not_visible_btn_sidebar(browser):
     elements_page = ElementsPage(browser)
